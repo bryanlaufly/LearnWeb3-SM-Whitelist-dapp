1bf555b39d61f21895a1fb8935663e3845de3d60
bryan <bryan_happy@hotmail.com>
2022-12-10 09:06:30 +0800

    Initial commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_KEY_URL = process.env.ALCHEMY_KEY_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: ALCHEMY_KEY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
